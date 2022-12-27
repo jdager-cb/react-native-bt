308028dae45544421a8e9f94d0d8732e7134dacc
Jose Miguel Dager Montoya <jdager@codebranh.co>
2022-12-27 09:35:43 -0500

    fix: event emitter

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import {NativeEventEmitter, NativeModules } from 'react-native';
+import { NativeEventEmitter, NativeModules } from 'react-native';
 const { Bt: TBBluetooth } = NativeModules;
 
 export default new NativeEventEmitter(TBBluetooth);
